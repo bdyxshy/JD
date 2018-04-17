a36a98c593694ec266ef92751613e982ed0467ee
bdyxshy <4763852@qq.com>
2018-04-17 17:16:22 +0800

    sql

--- JD/src/service/LoginService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import entity.User;
-
-public interface LoginService {
-	public User searchUserName(String regName);
-	public int addUser(String regName, String pwd);
-	public User validate(String loginname, String loginpwd);
-}
